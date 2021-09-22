f658909688e9c8464fcf68ed9f880b4b1d009698
phill-mutu <7622261+Lysias91@users.noreply.github.com>
2021-09-23 11:40:15 +1200

    add Home component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,19 @@ import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import Home from './Home'
+// import Register from './Register'
+// import Login from './Login'
 
 const App = () => {
   return (
     <>
       <h1>Welcome to Aawhina</h1>
       <Route exact path='/' component={Home} />
+      {/* <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} /> */}
     </>
   )
 }
 
 export default App
+// export default connect()(App)
