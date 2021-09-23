b728bd6d0767f698a78ef8e2cadd6bd60f995af1
phill-mutu <7622261+Lysias91@users.noreply.github.com>
2021-09-24 10:07:31 +1200

    merge register changes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import Welcome from './Welcome'
 import Register from './Register'
 import Dashboard from './Dashboard'
+import nav from './Nav'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
 
       <Router>
         <Route exact path='/' component={Welcome} />
+        <Route path='/dashboard' component={nav} />
         <Route exact path='/dashboard' component={Dashboard} />
       </Router>
     </>
--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,51 +1,34 @@
-
-// import React from 'react'
-
-// function Dashboard () {
-
-//   return (
-//     <>
-//       <p>hello</p>
-//     </>
-//   )
-// }
-
-// export default Dashboard
-
-import * as React from 'react';
-import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
-import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
-import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
-import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import * as React from 'react'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 
 export default function Dashboard() {
-  const [alignment, setAlignment] = React.useState('left');
+  const [userRole, setUserRole] = React.useState('offers')
 
-  const handleAlignment = (event, newAlignment) => {
-    setAlignment(newAlignment);
-  };
+  const handleUserRole = (event, newUserRole) => {
+    setUserRole(newUserRole)
+  }
 
   return (
     <ToggleButtonGroup
-      value={alignment}
+      value={userRole}
       exclusive
-      onChange={handleAlignment}
-      aria-label="text alignment"
-    >
-      <ToggleButton value="left" aria-label="left aligned">
-        <FormatAlignLeftIcon />
+      onChange={handleUserRole}
+      aria-label="userRole">
+      <ToggleButton 
+          size="large" 
+          color="primary" 
+          value="offers" 
+          aria-label="Offers">
+                Offers
       </ToggleButton>
-      <ToggleButton value="center" aria-label="centered">
-        <FormatAlignCenterIcon />
-      </ToggleButton>
-      <ToggleButton value="right" aria-label="right aligned">
-        <FormatAlignRightIcon />
-      </ToggleButton>
-      <ToggleButton value="justify" aria-label="justified" disabled>
-        <FormatAlignJustifyIcon />
+      <ToggleButton 
+          size="large" 
+          color="primary"
+          value="justify" 
+          aria-label="Requests"> 
+                Requests
       </ToggleButton>
     </ToggleButtonGroup>
-  );
+  )
 }
