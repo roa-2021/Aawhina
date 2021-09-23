551036f38dd5d323bc89d9805209c12f50f1baf9
zoe-joblin <zoe.joblin@gmail.com>
2021-09-23 18:58:54 +1200

    routes and db funcs

--- server/db/requests_db.js
+++ server/db/requests_db.js
@@ -7,20 +7,30 @@ const getAllRequestsAndUsersAndSuburbs = () => {
     .select('*', 'requests.id AS request_id')
 }
 // TODO //
-const getRequestById = () => {
+const getRequestById = (id) => {
   return db('requests')
+    .where('request.id', id)
+    .first()
 }
 // TODO //
-const deleteRequest = () => {
+const deleteRequest = (id) => {
   return db('requests')
+    .where('id', id)
+    .del()
 }
 // TODO //
-const createRequest = () => {
+const createRequest = (request) => {
   return db('requests')
+    .insert(request, 'id')
+    .then(requestId => {
+      return getRequestById(requestId[0])
+    })
 }
-// TODO //
-const editRequest = () => {
+
+const editRequest = (id, newRequest) => {
   return db('requests')
+    .where('id', id)
+    .update(newRequest)
 }
 
 module.exports = {
