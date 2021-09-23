b595d5766cc0fc3cf9145c262392c201a8ee58fe
zoe-joblin <zoe.joblin@gmail.com>
2021-09-23 14:41:22 +1200

    run migrate and seeds

--- server/db/seeds/02_suburbs.js
+++ server/db/seeds/02_suburbs.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('suburbs').del()
+  return knex('suburb').del()
     .then(function () {
       // Inserts seed entries
-      return knex('suburbs').insert([
+      return knex('suburb').insert([
         {id: 1, neighbours: []},
         {id: 2, neighbours: []},
         {id: 3, neighbours: []}
