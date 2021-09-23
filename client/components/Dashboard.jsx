0ba2340f8503e3400340304b4fcc9611cde48e65
Phillip Laughton-Mutu <phill@Phillips-MBP.localdomain>
2021-09-23 20:53:29 +1200

    test toggle button

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,13 +1,51 @@
 
-import React from 'react'
+// import React from 'react'
 
-function Dashboard () {
+// function Dashboard () {
+
+//   return (
+//     <>
+//       <p>hello</p>
+//     </>
+//   )
+// }
+
+// export default Dashboard
+
+import * as React from 'react';
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+export default function Dashboard() {
+  const [alignment, setAlignment] = React.useState('left');
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
 
   return (
-    <>
-      <p>hello</p>
-    </>
-  )
+    <ToggleButtonGroup
+      value={alignment}
+      exclusive
+      onChange={handleAlignment}
+      aria-label="text alignment"
+    >
+      <ToggleButton value="left" aria-label="left aligned">
+        <FormatAlignLeftIcon />
+      </ToggleButton>
+      <ToggleButton value="center" aria-label="centered">
+        <FormatAlignCenterIcon />
+      </ToggleButton>
+      <ToggleButton value="right" aria-label="right aligned">
+        <FormatAlignRightIcon />
+      </ToggleButton>
+      <ToggleButton value="justify" aria-label="justified" disabled>
+        <FormatAlignJustifyIcon />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
 }
-
-export default Dashboard
