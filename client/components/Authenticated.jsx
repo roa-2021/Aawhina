e10ee6cdd95a0f65b8b512b5f2928ae786bd6e8f
zoe-joblin <zoe.joblin@gmail.com>
2021-09-24 11:58:27 +1200

    add authenticated component

---
+++ client/components/Authenticated.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { isAuthenticated } from 'authenticare/client'
+
+export function IfAuthenticated ({ children }) {
+  return isAuthenticated()
+    ? <>{ children }</>
+    : null
+}
+
+export function IfNotAuthenticated ({ children }) {
+  return !isAuthenticated()
+    ? <>{ children }</>
+    : null
+}
