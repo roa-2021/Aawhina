ad964c9f38ab3fb0d7d7c22dce26fc77620bb4de
zoe-joblin <zoe.joblin@gmail.com>
2021-09-23 19:59:42 +1200

    finish offers routes

--- server/routes/offerRoutes.js
+++ server/routes/offerRoutes.js
@@ -26,4 +26,38 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+  const offer = req.body
+  db.createOffer(offer)
+    .then(offer => {
+      return res.json(offer)
+    })
+    .catch(error => {
+      res.status(500).json(`error did not work: ${error.message}`)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  db.deleteOffer(id)
+    .then(() => {
+      return res.json(`offer id number ${id} has been deleted`)
+    })
+    .catch(error => {
+      res.status(500).json(`error did not work: ${error.message}`)
+    })
+})
+
+router.patch('/:id', (req, res) => {
+  const id = req.params.id
+  const newOffer = req.body
+  db.editOffer(id, newOffer)
+    .then((id) => {
+      return res.json(`offer id number ${id} has been updated`)
+    })
+    .catch(error => {
+      res.status(500).json(`error did not work: ${error.message}`)
+    })
+})
+
 module.exports = router
