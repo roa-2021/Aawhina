a1d9c2a987e75c1ae5c5441e8de84cffe2b96d46
phill-mutu <7622261+Lysias91@users.noreply.github.com>
2021-09-23 12:19:38 +1200

    add login & Register components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,17 +2,12 @@ import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import Home from './Home'
-// import Register from './Register'
-// import Login from './Login'
+import Welcome from './Welcome'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Aawhina</h1>
-      <Route exact path='/' component={Home} />
-      {/* <Route path='/register' component={Register} />
-      <Route path='/login' component={Login} /> */}
+      <Route exact path='/' component={Welcome} />
     </>
   )
 }
