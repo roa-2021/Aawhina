cc43a282866ccb9d8a323803e3ddc9a176ac1ebe
Jo Tregidga <84287388+jo-tree@users.noreply.github.com>
2021-09-24 15:52:56 +1200

    auth stuff, not finished but heres the branch zoe

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import Login from './Login'
 import Register from './Register'
+import Link from 'react-router-dom'
 
 function Welcome () {
-  const [loginFormVisible, setLoginFormVisible] = useState(false)
-  const [registerFormVisible, setRegisterFormVisible] = useState(false)
+  // const [loginFormVisible, setLoginFormVisible] = useState(false)
+  // const [registerFormVisible, setRegisterFormVisible] = useState(false)
+  // const [logoutFormVisible, setLogoutFormVisible] = useState(false)
+
 
   // const toggleLoginForm = () => {
   //   setLoginFormVisible(true)
@@ -15,26 +18,32 @@ function Welcome () {
   //   setRegisterFormVisible(true)
   // }
 
-  const toggleLoginForm = () => {
-    setLoginFormVisible(!loginFormVisible)
-  }
+  // const toggleLoginForm = () => {
+  //   setLoginFormVisible(!loginFormVisible)
+  // }
 
-  const toggleRegisterForm = () => {
-    setRegisterFormVisible(!registerFormVisible)
-  }
+
+  // const toggleLogoutForm = () => {
+  //   setLogoutFormVisible(!logoutFormVisible)
+  // }
+
+
+  // const toggleRegisterForm = () => {
+  //   setRegisterFormVisible(!registerFormVisible)
+  // }
 
   return (
     <>
-      {
+      {/* {
         loginFormVisible
           ? <Login />
           : registerFormVisible
             ? <Register />
-            : <>
+            : <> */}
               <h3>Welcome Component</h3 >
-              <button onClick={toggleLoginForm}>Login</button>
-              <button onClick={toggleRegisterForm}>Register</button>
-            </>}
+              <Link to ><button>Login</button></Link>
+              <Link to ><button>Logout</button></Link>
+              <Link to ><button>Register</button></Link>
     </>
   )
 }
