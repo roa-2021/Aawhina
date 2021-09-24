3885a086177cda16e55feceb592a7f688c5c03cf
Matthew Searle <matthewjsearle@gmail.com>
2021-09-24 15:03:02 +1200

    tweaks to Nav

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -12,10 +12,9 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import { connect } from 'superagent';
+import { connect } from 'react-redux';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
