39572c07631a629c3d0aa88c322c641a87175a71
phill-mutu <7622261+Lysias91@users.noreply.github.com>
2021-09-25 20:48:30 +1200

    fix dev

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,24 +10,13 @@ import Box from '@mui/material/Box'
 import Requests from './Requests'
 // import Register from './Register'
 import Dashboard from './Dashboard'
-<<<<<<< HEAD
-import Footer from './Footer'
-import Nav from './Nav'
-||||||| aaab3e3
-import nav from './Nav'
-=======
->>>>>>> 1c2c59c5266bb1e7c5acd9bbeb5cf22647ad83be
 import Login from './Login'
 import Logout from './Logout'
-<<<<<<< HEAD
-import MakeProfile from './MakeProfile'
-||||||| 1c2c59c
-=======
 import MakeRequest from './MakeRequest'
+import MakeProfile from './MakeProfile'
 
 const App = (props) => {
   const { dispatch } = props
->>>>>>> 4bf92f78389c6f25187b73b3296fed44233e3484
 
   useEffect(() => {
     dispatch(getOffersThunk())
@@ -38,7 +27,7 @@ const App = (props) => {
   return (
     <>
       <CssBaseline/>
-      <Nav />
+      {/* <Nav /> */}
       <Box
       sx={{
         display: 'flex',
@@ -51,33 +40,14 @@ const App = (props) => {
         <Route exact path='/login' component={Login} />
         <Route exact path='/logout' component={Logout} />
         <Route exact path='/' component={Welcome} />
-<<<<<<< HEAD
-        <Route path='/requests' component={Requests} />
-        <Route exact path='/register' component={MakeProfile} />
-||||||| aaab3e3
-        <Route path='/dashboard' component={nav} />
-        <Route exact path='/register' component={MakeProfile} />
-=======
         <Route exact path='/makeprofile' component={MakeProfile} />
->>>>>>> 1c2c59c5266bb1e7c5acd9bbeb5cf22647ad83be
         <Route exact path='/dashboard' component={Dashboard} />
-<<<<<<< HEAD
-        <Route path='/' component={Footer} />
-||||||| 1c2c59c
-=======
         <Route exact path='/requests/new' component={MakeRequest} />
->>>>>>> 4bf92f78389c6f25187b73b3296fed44233e3484
       </Router>
       </Box>
     </>
   )
 }
 
-<<<<<<< HEAD
-export default connect()(App)
-||||||| 1c2c59c
-export default App
-=======
 
 export default connect()(App)
->>>>>>> 4bf92f78389c6f25187b73b3296fed44233e3484
