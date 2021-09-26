296b8c8a3eb103c93d782d530346e91c3befc843
Jo Tregidga <84287388+jo-tree@users.noreply.github.com>
2021-09-27 11:07:47 +1300

    writing profile to database, including suburbs

--- client/components/MakeProfile.jsx
+++ client/components/MakeProfile.jsx
@@ -11,6 +11,7 @@ import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel';
+import ListSubHeader from '@mui/material/ListSubheader'
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import Grid from '@mui/material/Grid'
@@ -29,14 +30,9 @@ function MakeProfile (props) {
   const { dispatch } = props
   const { user } = useAuth0()
   
-  const [newSuburb, setSuburb] = useState([])
   const [theSuburbs, setSuburbs] = useState([])
-
-  useEffect(() => {
-    getSuburbs()
-    .then(res => setSuburbs(res)
-    )
-  },[])
+  const [gender, setGender] = useState('')
+  
   
   const [values, setValues] = useState({
     first: '',
@@ -44,7 +40,34 @@ function MakeProfile (props) {
     email: '',
     bio: '',
   })  
-  const [gender, setGender] = useState('')
+  
+  useEffect(() => {
+    getSuburbs()
+    .then(res => setSuburbs(res)
+    )
+  },[])
+  
+  const [newSuburb, setSuburb] = useState(0)
+
+    const handleSuburb = (e) => {
+      e.preventDefault()
+      setSuburb(e.target.value)
+    }  
+  
+    const handleGender =(e) => {
+      e.preventDefault()
+      setGender(e.target.value)
+  
+    }  
+  
+    const handleChange =(e) => {
+      e.preventDefault()
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value
+      })  
+  
+    }  
 
   // const [image, setImage ] = useState("")
   // const [ url, setUrl ] = useState("")
@@ -71,26 +94,6 @@ function MakeProfile (props) {
   //     display: 'none',
   //   })
 
-
-  const handleSuburb = (e) => {
-    e.preventDefault()
-    setSuburb([e.target.value])
-  }  
-
-  const handleGender =(e) => {
-    e.preventDefault()
-    setGender(e.target.value)
-
-  }  
-
-  const handleChange =(e) => {
-    e.preventDefault()
-    setValues({
-      ...values,
-      [e.target.name]: e.target.value
-    })  
-
-  }  
   let history = useHistory()
 
   const handleSubmit = (e) => {
@@ -104,6 +107,9 @@ function MakeProfile (props) {
        gender: gender, 
 
      }  
+
+     console.log(newUser)
+     console.log(newSuburb)
       dispatch(postUserThunk(newUser)) 
       history.push('/') 
     
@@ -116,11 +122,11 @@ function MakeProfile (props) {
 
   return (
     <>
-    <Nav/>
     <Container component='main' maxWidth='xs'>
       <Box sx={{ marginTop:8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <Typography  component='h1' variant='h5'>Create Profile</Typography>
         <Box sx={{mt:3}} component='form'>
+      
           <Grid container spacing={2} >
 
           <Grid item xs={6}>
@@ -132,19 +138,40 @@ function MakeProfile (props) {
           <Grid item xs={12}>
             <TextField sx={{mt:3 }} required fullWidth id='outlined-required' defaultValue={user.email} label='Email' name='email' onChange={handleChange}/>
           </Grid>
+       
           <Grid item xs={6}>
+
           <InputLabel>Suburb</InputLabel>
           <Select
-            label='Suburb'
-            value={newSuburb}
+            defaultValue=''
             onChange={handleSuburb}
-            sx={{ width: '21ch' }}>
-              {theSuburbs && theSuburbs.map(s => (
-          <MenuItem value={s.id}>{s.name}</MenuItem>
-              ))}
+            value={newSuburb}
+            sx={{ width: '21ch' }}
+            label='Suburb'>
+             
+
+            <ListSubHeader>Wellington</ListSubHeader>
+            {theSuburbs && theSuburbs.filter(s => s.region === 'Wellington').map(s => ( 
+            <MenuItem value={s.id}>{s.name}</MenuItem>
+            ))}
+            <ListSubHeader>Lower Hutt</ListSubHeader>
+            {theSuburbs && theSuburbs.filter(s => s.region === 'Lower Hutt').map(s => (
+            <MenuItem value={s.id}>{s.name}</MenuItem>
+            ))}
+
+
+            <ListSubHeader>Upper Hutt</ListSubHeader>
+            {theSuburbs && theSuburbs.filter(s => s.region === 'Upper Hutt').map(s => (
+            <MenuItem value={s.id}>{s.name}</MenuItem>
+            ))}
+
+            <ListSubHeader>Porirua</ListSubHeader>
+            {theSuburbs && theSuburbs.filter(s => s.region === 'Porirua').map(s => (
+            <MenuItem value={s.id}>{s.name}</MenuItem>
+            ))}
+
           </Select>
           </Grid>
-          
           <Grid item xs={6}>
           <InputLabel>Gender</InputLabel>
             <Select
@@ -159,7 +186,7 @@ function MakeProfile (props) {
             </Select>
         
           </Grid>
-      
+  
         <Grid item xs={12}>
           <TextField sx={{ mt: 4 }} required fullWidth id = 'outlined-required' multiline rows={6}  label = 'About you' name='bio' value={bio} onChange={handleChange}/>
         </Grid>
@@ -197,26 +224,14 @@ export default connect(mapState2Props)(MakeProfile)
 
 
 
-//potential workaround to filter by region 
+// <Select
+// label='Suburb'
+// value={newSuburb}
+// onChange={handleSuburb}
+// sx={{ width: '21ch' }}>
+//   {theSuburbs && theSuburbs.map(s => (
+// <MenuItem value={s.id}>{s.name}</MenuItem>
+//   ))}
 
-// <optgroup label='Wellington'>
-// {newSuburb && newSuburb.map(s => (
-// <option value={s.id}>{s.name}</option>
-// ))}
-// </optgroup>
-{/* <optgroup label='Lower Hutt'>
-{newSuburb && newSuburb.map(s => (
-<option value={s.id}>{s.name}</option>
-))}
-</optgroup>
 
-<optgroup label='Upper Hutt'>
-{newSuburb && newSuburb.map(s => (
-<option value={s.id}>{s.name}</option>
-))}
-</optgroup>
-<optgroup label='Porirua'>
-{newSuburb && newSuburb.map(s => (
-<option value={s.id}>{s.name}</option>
-))}
-</optgroup> */}
+
