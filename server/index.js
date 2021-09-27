eb6470293108fd0292b8822d5b69a47cfc7c4008
zoe-joblin <zoe.joblin@gmail.com>
2021-09-27 19:54:57 +1300

    fix issues for deplyment

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const server = require('./server')
 
 const port = process.env.PORT || 3000
 
+if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if(envConfig.error) throw envConfig.error
+}
+
 server.listen(port, function () {
   // eslint-disable-next-line no-console
   console.log('Listening on port', port)
