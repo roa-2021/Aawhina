5afd99dc89ce91f5c347a3a6d16c2dcc8da8cff4
Matthew Searle <matthewjsearle@gmail.com>
2021-09-27 23:35:18 +1300

    more merge

--- client/components/Requests.jsx
+++ client/components/Requests.jsx
@@ -18,65 +18,6 @@ function Requests ({ currentUser, requests }) {
 
   const requestsToShow = currentUser ? requests.filter(request => currentUser.id === request.user_id) : requests
 
-<<<<<<< HEAD
-  return (
-    <>
-      <Container 
-        component="main"
-        maxWidth="md" 
-      >
-       { !currentUser && <Box mt={4} >
-          <Typography variant="h5" align="center">
-            These neighbours of yours have requested help:
-          </Typography>
-        </Box>
-        }
-        <Box mt={2}>
-          <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            aligncards="stretch"
-            rowSpacing={2} 
-          >
-            { requests && requestsToShow.map(request => <RequestCard key={request.id} request={request}/>)}
-            <Grid 
-              item
-              my={2}
-              sx={{
-                display: 'flex',
-                justifyContent: 'center'
-              }}
-||||||| 064dbfa
-  return (
-    <>
-      <Container 
-        component="main"
-        maxWidth="md" 
-      >
-       { !currentUser && <Box mt={4} >
-          <Typography variant="h5" align="center">
-            These neighbours of yours have requested help:
-          </Typography>
-        </Box>
-        }
-        <Box mt={2}>
-          <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            aligncards="stretch"
-            rowSpacing={2} 
-          >
-            { requests && requestsToShow.map(request => <RequestCard key={request.id} request={request} currentUser={currentUser}/>)}
-            <Grid 
-              item
-              mt={2}
-              sx={{
-                display: 'flex',
-                justifyContent: 'center'
-              }}
-=======
   if (isAuthenticated) {
     return (
       <>
@@ -97,7 +38,6 @@ function Requests ({ currentUser, requests }) {
               justifyContent="center"
               aligncards="stretch"
               rowSpacing={2} 
->>>>>>> 9d77adbb234122aa8a86b7549b6130e61d5512bb
             >
               { requests && requestsToShow.map(request => <RequestCard key={request.id} request={request} currentUser={currentUser}/>)}
               <Grid 
