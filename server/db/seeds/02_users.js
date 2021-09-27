c15a524317bcbf5e2b96fdc30b9460e979939abe
zoe-joblin <zoe.joblin@gmail.com>
2021-09-28 10:43:37 +1300

    add empty seeds file

--- server/db/seeds/02_users.js
+++ server/db/seeds/02_users.js
@@ -1,7 +1,7 @@
 exports.seed = (knex) => {
   // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
+  // return knex('users').del()
+  //   .then(function () {
       // Inserts seed entries
       return knex('users').insert([
         { id: 1, first_name: 'Jo', last_name: 'T', email: 'jo@gmail.com', suburb_id: 1, bio: 'Hi, I"m Jo!', gender: 'female', spoken_languages: 'English', image: ''},
@@ -9,5 +9,6 @@ exports.seed = (knex) => {
         { id: 3, first_name: 'Zoe', last_name: 'J', email: 'zoe@gmail.com', suburb_id: 3, bio: 'Hi, I"m Zoe!', gender: 'gender non conforming', spoken_languages: 'English', image: ''},
         { id: 4, first_name: 'Matt', last_name: 'S', email: 'matt@gmail.com', suburb_id: 1, bio: 'Hi, I"m Matt!', gender: 'male', spoken_languages: 'English', image: ''}
       ])
-    })
-   }
+    }
+  //   )
+  //  }
