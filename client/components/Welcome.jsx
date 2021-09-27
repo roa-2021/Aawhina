e31ea56a7559ad15a3996ca3143545c14ef5d81e
Jo Tregidga <84287388+jo-tree@users.noreply.github.com>
2021-09-27 22:19:33 +1300

    photos to homepage, pondering a new idea. to discuss

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -30,42 +30,24 @@ function srcset(image, size, rows = 1, cols = 1) {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    img: './images/neighbours.jpg',
     title: 'Breakfast',
     rows: 2,
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
+    img: './images/happy.jpg',
   },
   {
     img: './images/background.png',
-    title: 'Basketball',
+    cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    img: './images/grocery.jpg',
+    cols: 2,
+  },
+  {
+    img: './images/apples.jpeg',
     title: 'Fern',
   },
 ]
@@ -97,16 +79,16 @@ function Welcome (props) {
   return (
 
    <>
-      <Stack sx={{ pt: 10 }}direction="row" justifyContent="center">
+      <Stack sx={{ pt: 10, pr: 20 }}direction="row" justifyContent="right">
         <Typography  component='h1' variant='h3'>Welcome to Āwhina!</Typography>
       </Stack>
-      <Stack sx={{ pt: 20 }}direction="row" spacing={25}justifyContent="center">
+      <Stack sx={{ pt: 20, pr: 17 }}direction="row" spacing={25}justifyContent="right">
         <Login/>
         <Register/>
       </Stack>
 
       <ImageList
-      sx={{ width: 500, height: 500 }}
+      sx={{ pl: 10, pb: 60, width: 680, height: 500 }}
       variant="quilted"
       cols={4}
       rowHeight={121}
