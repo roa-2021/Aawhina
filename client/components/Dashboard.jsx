85627c59a4c367bc5a9dec395a9f9898541b54ad
zoe-joblin <zoe.joblin@gmail.com>
2021-09-29 20:37:03 +1300

    fixes for heroku

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -52,7 +52,7 @@ function Dashboard({ currentUser }) {
         </Container>
         {offers
           ? <Offers />
-          : <Requests />
+          : <Requests currentUser={currentUser} />
         }
       </>
     )
