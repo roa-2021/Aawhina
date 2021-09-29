0be1e8985f81d39e2e7fe425a9ae0e43b0a09111
zoe-joblin <zoe.joblin@gmail.com>
2021-09-29 19:01:31 +1300

    fix suburb db stuff for pstgres

--- server/db/suburb_db.js
+++ server/db/suburb_db.js
@@ -3,16 +3,16 @@ const db = require('./connection')
 function getAllSuburbs () {
   return db('suburb')
 
-    .then(response => {
-      suburbsArr = []
-      response.map(suburb => {
+    // .then(response => {
+    //   suburbsArr = []
+    //   response.map(suburb => {
 
-        newSuburb = convertNeighboursStringToArray(suburb)
+    //     newSuburb = convertNeighboursStringToArray(suburb)
         
-        suburbsArr.push(newSuburb)
-      })
-      return suburbsArr
-    })
+    //     suburbsArr.push(newSuburb)
+    //   })
+    //   return suburbsArr
+    // })
 
 }
 
@@ -20,7 +20,7 @@ function getSuburb (id) {
   return db('suburb')
     .where('suburb.id', id)
     .first()
-    .then(suburb => convertNeighboursStringToArray(suburb))
+    // .then(suburb => convertNeighboursStringToArray(suburb))
 }
 
 const convertNeighboursStringToArray = (suburb) => {
