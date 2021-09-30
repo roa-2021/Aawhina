4c17f2219e39bade61b33b735565f9c39dc47fcf
Matthew Searle <matthewjsearle@gmail.com>
2021-09-30 15:20:32 +1300

    sassy

--- server/db/suburb_db.js
+++ server/db/suburb_db.js
@@ -2,12 +2,23 @@ const db = require('./connection')
 
 function getAllSuburbs () {
   return db('suburb')
+    .then(response => {
+      suburbsArr = []
+      response.map(suburb => {
+        convertNeighboursStringToArray(response)
+        suburbsArr.push(newSuburb)
+      })
+      return suburbsArr
+    })
 }
 
 function getSuburb (id) {
   return db('suburb')
     .where('suburb.id', id)
     .first()
+    .then(response => {
+      convertNeighboursStringToArray(response)
+    })
 }
 
 const convertNeighboursStringToArray = (suburb) => {
